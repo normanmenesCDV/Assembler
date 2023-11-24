634b133af93b4cc2ef3a99a6a0c6441df403fff6
Norman Menes <nmenes@edu.cdv.pl>
2023-11-24 16:28:39 +0100

    dalsze prace nad lab 4

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ __end: nop
 ```
 Między tymi etykietami mamy umieszczać wszystkie instrukcje.
 Jest do wymagane do testów jednostkowych prowadzącego zajęcia.
+
+## Uruchamianie
+1. Utwórz na Linuxie plik `program.s` i wklej do niego kod źródłowy
+2. Uruchom:
+```
+$ as --32 -g program.s -o ass.o && ld -melf_i386 ass.o && gdb a.out
+```
+3. By wyświetlić kod, wpisz: `l`
+4. Utwórz breakpoint w dowolnym miejscu kodu, wpisz: `b x`, gdzie `x` to numer linii kodu (linia na której chcesz ustawić punkt przerwania, musi zaweirać operację, może to być np. `nop`)
+5. By uruchomić, wpisz: `r`
+6. By wyświetlić wartość rejestrów, wpisz: `i r`
+7. By zamknąć, wpisz: `q`
--- solutions/TODO-MenesN_4.2.s
+++ solutions/TODO-MenesN_4.2.s
@@ -2,23 +2,30 @@
 .data
 
 array: .int 2, 8, -5, 10, -6, 5, 0, 7
-arrayLength: .int (arrayLength-array) / 4
+array_end:  # ten element zostanie zapisany w pamięci zaraz po tablicy `array`
+array_bytes: .int array_end - array
+array_size: .int (array_end-array) / 4 # Dzielimy przez 4 ze względu na 32-bitową architekturę
 
 .globl _start
 .text
 _start:
 __begin:
 
-mov $0, %EAX
-mov $array, %EBX       # Licznik indeksu
-mov arrayLength, %ECX  # Ilości elementów w tablicy
+mov  $0, %EAX               # liczba znalezionych elementów spełniających warunek w tablicy
+mov  $0, %EBX               # rejstr do operacji arytmetycznej na wskaźnikach
+mov  $array_bytes, %ECX     # rozmiar bitowy tablicy
 
-mov $0, %EAX
-
-.petla
-    cmp $0, (%EBX)
-    jle .not_positive
-    inc %EAX
-
+petla:
+    cmp  %ECX, %EBX
+    jg   endPetla
+    cmp  $0, array(%EBX)
+    jg   znalezionoElement
+    jmp  endif
+    znalezionoElement:
+        inc  %EAX
+    endif:
+    add  $4, %EBX           # Dodajemy 4 ze względu na 32-bitową architekturę
+    jmp  petla
+endPetla:
 
 __end: nop
